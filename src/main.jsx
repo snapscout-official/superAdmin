21e8143bcf7d4291be45457cb43c81d0d0810554
kliea <klinthjerald.matugas@carsu.edu.ph>
2023-10-21 21:14:21 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Trial from './pages/trial';
+import './assets/styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Trial />
+	</React.StrictMode>
+);
---
+++ src/pages/trial.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function trial() {
+	return (
+		<div>
+			<h1 className='text-red-500 text-3xl bg-gray-100'>
+				Hello, Tailwind CSS!
+			</h1>
+		</div>
+	);
+}
