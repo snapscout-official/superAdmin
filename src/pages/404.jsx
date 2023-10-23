43d2b082678bd5287867b143aeecc634f7968c88
kliea <klinthjerald.matugas@carsu.edu.ph>
2023-10-23 16:55:35 +0800

    test deploy

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,6 +4,7 @@ import Container from '../layouts/Index';
 export default function NotFound() {
 	return (
 		<Container>
+			{/* push test */}
 			<div className='flex justify-center items-center '>404</div>
 		</Container>
 	);
